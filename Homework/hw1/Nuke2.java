96ad4efcc7ef05da7d82418ad1a2f503a6db1b42
Sen <senonli6510@gmail.com>
2017-01-09 22:44:11 -0500

    Lab2, Fraction

--- Homework/hw1/Nuke2.java
+++ Homework/hw1/Nuke2.java
@@ -8,8 +8,9 @@ class Nuke2{
 		String s = keyboard.readLine();
 		String ss0 = s.substring(0, 1);
 		
-		String ss2ToEnd = s.substring(2, s.length());
-		String noSecond = ss0.concat(ss2ToEnd);
+		//String ss2ToEnd = s.substring(2, s.length());
+		String ss2ToEnd = s.substring(2);
+		String noSecond = ss0 + ss2ToEnd;
 		
 		// System.out.println("ss0 = " + ss0);
 		// System.out.println("s.length() = " + s.length());
